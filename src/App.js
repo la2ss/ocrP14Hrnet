ccf7548f4265c9ce30fd8dced811bc8a7f11926a
Lassina Sylla <lass390225@gmail.com>
2024-01-11 20:16:03 +0100

    Maj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Routes, Route } from 'react-router';
 import './App.css';
+import Homepage from './pages/Homepage';
+import EmployeeList from './pages/EmployeList';
 import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Navbar />
-      </header>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Homepage />}/>
+      <Route path='/Employee' element={<EmployeeList />} />
+    </Routes>
     </div>
   );
 }
