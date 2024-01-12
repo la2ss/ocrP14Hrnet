b24b11de2a7b9647e7b697e71637201fe197b522
Lassina Sylla <lass390225@gmail.com>
2024-01-12 12:20:53 +0100

    Maj data

---
+++ src/assets/data/departements.js
@@ -0,0 +1,16 @@
+export const departements = [{
+    "name": "Sales",
+    "abréviation": "SLS"
+},{
+    "name": "Marketing",
+    "abréviation": "MKT"
+},{
+    "name": "Engineering",
+    'abréviation': "EGR"
+},{
+    "name": "Human Ressources",
+    "abréviation": "HRS"
+},{
+    "name": "Legal",
+    "abréviation": "LGL"
+}]
